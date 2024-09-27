a8afcfc18d050fde71dd9ea9ab6170dd60d8b746
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-09-27 17:36:18 +0000

    DataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("VIKRANT SINGH"); //we using nodejs, not browser
+
+console.log(3
+    +
+3)// code readability should be high
+
+let name="Vikrant"
+let age=20
+let isLoggedIn = false
+let state = null
+
+
+//number =>2 to power 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+
+
+//object
+
+console.log(typeof null);
