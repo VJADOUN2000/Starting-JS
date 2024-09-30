142cc938c1190f2395d42e4da688e633b3e450e9
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-09-30 17:02:01 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,31 @@
+let score= null;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+let val = 123;
+let StringValue= String(val);
+let bol =0;
+let booleanValue =Boolean(bol);
+
+//console.log(StringValue);
+//console.log(valueInNumber);
+//console.log(typeof score);
+//console.log(typeof booleanValue);
+//console.log(booleanValue);
+
+// "33" => 33
+//"33abc" => NAN
+// true => 1
+// false => 0
+//null => 0
+//undefined => NAN
+// 1 => true
+// 0 => false
+
+let isLogged = undefined;
+
+let booleanIsLoggedIn= Boolean(isLogged);
+
+console.log(booleanIsLoggedIn);
