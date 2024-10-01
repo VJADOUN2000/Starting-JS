e7f15984c0a477edc8a40f3bb727c1703bbc829e
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-10-01 17:56:30 +0000

    Comparison done 60-70%

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(5>=4);
+// console.log(5<=4);
+
+//console.log("2">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+ console.log(null>=0);
+ console.log(null<=1);
+
+// console.log(undefined<0)> //false
+// console.log(undefined<0);  //false
+// console.log(undefined==0);  //false
+
+//===
+
+console.log("2"===2); //false => also check datatypes
