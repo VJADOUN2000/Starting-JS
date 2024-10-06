1407b2477244dc8ccbcf158282dfad7925b5c0f1
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-10-06 16:59:21 +0000

    Nums done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,23 @@
+const score=56;
+//console.log(score);
+
+const balance = 100
+//console.log(balance);
+
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+
+const anotherNum = 1456.8966;
+
+//console.log(anotherNum.toPrecision(3))
+
+//console.log(typeof balance);
+
+const hundreds = 1000000;
+
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++
+
