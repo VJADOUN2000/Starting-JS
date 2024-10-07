3af42cf0baed49f46edaddbd2ac5cf592e1beb96
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-10-07 17:16:58 +0000

    Math done

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -21,3 +21,21 @@ const hundreds = 1000000;
 
 //+++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++
 
+// console.log(Math);
+// console.log(Math.abs(-954));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(45,94,421));
+
+//console.log(Math.random());
+
+//console.log(Math.floor(Math.random()*10)+1);
+
+//console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=30;
+
+console.log(Math.floor(Math.random()* (max-min +1))+min);
