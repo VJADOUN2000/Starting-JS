b90aa05ff79dd3b851f658cb0b60fb8a58a59683
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-10-08 17:01:55 +0000

    Dates done partially

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,17 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString);
+
+console.log(typeof myDate);
+
+//let newDate = new Date(2026,5,2)
+//let newDate = new Date(2026,5,2,2,50)
+let newDate = new Date("2024-06-02")
+
+console.log(newDate.toLocaleString());
