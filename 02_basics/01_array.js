a5172a01793fc2e4936accb0780bd9a35ffbef77
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2024-10-12 16:41:20 +0000

    Array part1 done( Methods,slice,splice)

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,5 +1,34 @@
-//array
+// //array
 
-const myArr =[0,1,2,3,4,5,true,"vikrant"];
+// //const myArr =[0,1,2,3,4,5,true,"vikrant"];
 
-console.log(myArr[8]);
+  const myArr = new Array(0,45,546,5,123)
+
+// // //Array Methods
+
+// //  myArr.push(6);
+// //  myArr.pop();
+
+// //  console.log(myArr.includes(9))
+// //  console.log(myArr.indexOf(1232))
+
+// //myArr.unshift(9);
+// //myArr.shift();
+
+// const newArr = myArr.joinn();
+
+// console.log(myArr);       
+// console.log(typeof newArr);   
+
+// slice , splice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3);
+console.log("b",myArr);
+console.log(myn1);
+
+console.log("c",myArr);
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("D", myArr);  
