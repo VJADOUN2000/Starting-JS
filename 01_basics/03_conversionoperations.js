3aee27eb8004ffac79cf5ae763868f65aebc6783
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2025-01-04 04:32:00 +0000

    Complete Revisionn Conversion

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,79 +1,109 @@
-let score= null;
+// let score= null;
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// //console.log(typeof score);
+// //console.log(typeof(score));
 
-let valueInNumber = Number(score);
-let val = 123;
-let StringValue= String(val);
-let bol =0;
-let booleanValue =Boolean(bol);
+// let valueInNumber = Number(score);
+// let val = 123;
+// let StringValue= String(val);
+// let bol =0;
+// let booleanValue =Boolean(bol);
 
-//console.log(StringValue);
-//console.log(valueInNumber);
-//console.log(typeof score);
-//console.log(typeof booleanValue);
-//console.log(booleanValue);
+// //console.log(StringValue);
+// //console.log(valueInNumber);
+// //console.log(typeof score);
+// //console.log(typeof booleanValue);
+// //console.log(booleanValue);
 
-// "33" => 33
-//"33abc" => NAN
-// true => 1
-// false => 0
-//null => 0
-//undefined => NAN
-// 1 => true
-// 0 => false
+// // "33" => 33
+// //"33abc" => NAN
+// // true => 1
+// // false => 0
+// //null => 0
+// //undefined => NAN
+// // 1 => true
+// // 0 => false
 
-let isLogged = undefined;
+// let isLogged = undefined;
 
-let booleanIsLoggedIn= Boolean(isLogged);
+// let booleanIsLoggedIn= Boolean(isLogged);
 
-//console.log(booleanIsLoggedIn);
+// //console.log(booleanIsLoggedIn);
 
 
-//***************************     Operations  ************************************ */
+// //***************************     Operations  ************************************ */
 
-let value =3 ;
-let negValue =-value;
-//console.log(negValue);
+// let value =3 ;
+// let negValue =-value;
+// //console.log(negValue);
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2/2);
-// console.log(2**4);
-// console.log(2%12);
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2/2);
+// // console.log(2**4);
+// // console.log(2%12);
 
-let str1 = "Hello";
+// let str1 = "Hello";
 
-let str2 = " VIKRANT";
+// let str2 = " VIKRANT";
 
-let str3 = str1+ str2;
+// let str3 = str1+ str2;
 
-// console.log(str3);
+// // console.log(str3);
 
-// console.log("2"+ 4+56);
-// console.log(5+5+"6");
+// // console.log("2"+ 4+56);
+// // console.log(5+5+"6");
 
-// console.log(+true);
-// console.log(+"");
+// // console.log(+true);
+// // console.log(+"");
 
-let num1, num2, num3
+// let num1, num2, num3
 
-num1=num2=num3 = 2 + 2;
+// num1=num2=num3 = 2 + 2;
 
-let gameCounter = 100;
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100;
+// ++gameCounter;
+// console.log(gameCounter);
 
-let x = 3;
-const y = x++;
+// let x = 3;
+// const y = x++;
 
-console.log(`x:${x}, y:${y}`);
-// Expected output: "x:4, y:3"
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
 
-let a = 3;
-const b = ++a;
+// let a = 3;
+// const b = ++a;
 
-console.log(`a:${a}, b:${b}`);
-// Expected output: "a:4, b:4"
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+/*********************************Revision*********************************/
+
+let run = true;
+
+console.log(run);
+console.log(typeof run);
+console.log(typeof (run));
+
+let runInNumber = Number(run);
+
+console.log(typeof runInNumber);
+console.log(runInNumber);
+
+/****************Coversion to Number********** */
+
+// "33" => Number
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+//false => 0
+//true => 1
+
+
+let isLoggedIn = "";
+
+let booleanvalue = Boolean(isLoggedIn);
+
+console.log(booleanvalue);
