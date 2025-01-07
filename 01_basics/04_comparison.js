21ae000e381f7bca5e8aabe50e1b0149fe82d9bf
VJADOUN2000 <140134700+VJADOUN2000@users.noreply.github.com>
2025-01-07 17:47:04 +0000

    Comparison Revision

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -2,18 +2,19 @@
 // console.log(5>=4);
 // console.log(5<=4);
 
-//console.log("2">1);
+//console.log("2">3);  //Convert string to number
 
+/***********************complex Comparsion Operation**************/
 
 // console.log(null>0);
-// console.log(null==0);
+ console.log(null==undefined);
  console.log(null>=0);
  console.log(null<=1);
 
-// console.log(undefined<0)> //false
-// console.log(undefined<0);  //false
-// console.log(undefined==0);  //false
+ console.log(undefined<0)> //false
+ console.log(undefined<0);  //false
+ console.log(undefined==0);  //false
 
-//===
+//------------------------------------------------------//
 
-console.log("2"===2); //false => also check datatypes
+console.log(2===2.0); //false => also check datatypes 
